69953a9cff89ef7d239efd7192c1209df401122e
Jeff Dupont <jdupont@getcake.com>
2018-07-06 17:33:38 -0700

    working on the score logic

--- internal/die/die.go
+++ internal/die/die.go
@@ -1,7 +1,6 @@
 package die
 
 import (
-	"fmt"
 	"math/rand"
 )
 
@@ -24,27 +23,64 @@ func (d *Die) Roll() {
 // Tally the score
 func Tally(dice []*Die) int {
 	total := 0
-	present := map[int]int{}
+	scored := []*Die{}
+	remain := []*Die{}
 
-	for d := range dice {
-		if _, ok := present[dice[d].Value]; !ok {
-			present[dice[d].Value] = 0
+	if len(dice) == 5 {
+		// run !
+		if dice[0].Value < dice[1].Value && dice[1].Value < dice[2].Value && dice[2].Value < dice[3].Value && dice[3].Value < dice[4].Value {
+			scored = dice
+			total += 1500
+			dice = remain
+			return total
 		}
-		present[dice[d].Value]++
-	}
 
-	// 5 of a kind
-	if len(dice) == 5 && len(present) == 1 {
-		if dice[0].Value == 1 {
-			total += 10000
-		} else {
-			total += dice[0].Value * 1000
+		// 5 of a kind !
+		if dice[0].Value == dice[1].Value && dice[1].Value == dice[2].Value && dice[2].Value == dice[3].Value && dice[3].Value == dice[4].Value {
+			scored = dice
+			if dice[0].Value == 1 {
+				total += 1000
+			} else {
+				total += dice[0].Value * 1000
+			}
+			dice = remain
+			return total
 		}
-	}
 
-	fmt.Printf("\nnumber of values: %d ", len(present))
-	for d, count := range present {
-		fmt.Printf("\n%d: %d ", d, count)
+		// 3 of a kind
+		if dice[0].Value == dice[1].Value && dice[1].Value == dice[2].Value {
+			scored = append(scored, dice[:2]...)
+			remain = dice[3:]
+			if dice[0].Value == 1 {
+				total += 1000
+			} else {
+				total += dice[0].Value * 100
+			}
+			dice = remain
+		} else if dice[1].Value == dice[2].Value && dice[2].Value == dice[3].Value {
+			scored = append(scored, dice[1:3]...)
+			remain = []*Die{dice[0], dice[4]}
+			total += dice[1].Value * 100
+			dice = remain
+		} else if dice[2].Value == dice[3].Value && dice[3].Value == dice[4].Value {
+			scored = append(scored, dice[2:]...)
+			remain = []*Die{dice[0], dice[1]}
+			total += dice[2].Value * 100
+			dice = remain
+		}
+
+		// all the rest
+		for d := range dice {
+			if dice[d].Value == 1 {
+				total += 100
+				scored = append(scored, dice[d])
+			} else if dice[d].Value == 5 {
+				total += 50
+				scored = append(scored, dice[d])
+			} else {
+				remain = append(remain, dice[d])
+			}
+		}
 	}
 
 	return total
